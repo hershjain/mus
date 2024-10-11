54f14c90c27873053d5148bfc79cb925bd481442
Shiva Vemireddy <shiva@nirva.ai>
2024-10-11 11:13:12 -0700

    fixed router bugs

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faMusic, faUser, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
         <nav>
             <ul>
-                <li><Link to="/discovery">Discovery</Link></li>
-                <li><Link to="/library">Library</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/create-playlist">Create Playlist</Link></li>
+                <li><Link to="/discovery"> <FontAwesomeIcon icon={faHome} size="26px" color='black' /> <a href="/discovery">Discovery</a> </Link></li>
+                <li><Link to="/library"> <FontAwesomeIcon icon={faMusic} size="26px" color='black'/> <a href="/library">Library</a> </Link></li>
+                <li><Link to="/profile">  <FontAwesomeIcon icon={faUser} size="26px" color='black'/> <a href="/profile">Profile</a> </Link></li>
+                <li><Link to="/create-playlist"> <FontAwesomeIcon icon={faPlusCircle} size="26px" color='black'/> <a href="/create-playlist">Create Playlist</a> </Link></li>
             </ul>
         </nav>
     );
