7afb6834e59613887aee5c5361e98705badba08b
Harsh Jain <hershjain@gmail.com>
2024-10-26 19:51:54 -0400

    add playlist component js

---
+++ src/components/playlist-component.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const PlaylistComponent = () => {
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/playlists/', {
+      credentials: 'include' // To include cookies for session handling
+    })
+      .then(response => response.json())
+      .then(data => setPlaylists(data.items || []))
+      .catch(error => console.error('Error fetching playlists:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Your Spotify Playlists</h1>
+      <ul>
+        {playlists.map((playlist) => (
+          <li key={playlist.id}>
+            <img src={playlist.images[0].url} alt={playlist.name} width={100} />
+            <p>{playlist.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PlaylistComponent;
