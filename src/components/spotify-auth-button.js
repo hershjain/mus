429abe91cb27f01672f105f27167b83255b9f020
Harsh Jain <hershjain@gmail.com>
2024-10-26 18:58:10 -0400

    updated auth so it comes back to profile

--- src/components/spotify-auth-button.js
+++ src/components/spotify-auth-button.js
@@ -4,17 +4,17 @@ import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 import '../styles/spotify-auth-button.css';
 
 const SpotifyAuthButton = () => {
-  const CLIENT_ID = "your_spotify_client_id"; // Replace with your actual client ID
-  const REDIRECT_URI = "http://localhost:3000/callback"; // Replace with your redirect URI
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
+  //const CLIENT_ID = "your_spotify_client_id"; // Replace with your actual client ID
+  //const REDIRECT_URI = "http://localhost:3000/callback"; // Replace with your redirect URI
+  //const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  //const RESPONSE_TYPE = "token";
 
   const handleSpotifyLogin = () => {
     // Build the Spotify authentication URL
     //const authURL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=${RESPONSE_TYPE}&scope=user-read-private user-read-email`;
     
     // Redirect to Spotify login
-    window.location.href = 'http://localhost:8000/spotify/login/' ;
+    window.location.href = 'http://localhost:8000/spotify/auth/login' ;
   };
 
   return (
