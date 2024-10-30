4ab997c03aca16392343650c0c548c3f0f54ad53
Harsh Jain <hershjain@gmail.com>
2024-10-29 22:55:28 -0400

    created ProtectedRoute.js, updated App.js - updated authentication so that you can't access app unless you log in. You're taken back to landing page if you try to access through url

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MainApp from "./MainApp";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+    const isAuthenticated = !!localStorage.getItem('access');
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path='/register' element={<Register />} />
-                <Route path="/app/*" element={<MainApp />} />
+                {/* Protect routes by nesting them under ProtectedRoute */}
+                <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+                    <Route path="/app/*" element={<MainApp />} />
+                    {/* Add more protected routes as needed */}
+                </Route>
             </Routes>
         </Router>
     );
