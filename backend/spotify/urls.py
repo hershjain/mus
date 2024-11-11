d937cdd2d79c343d0ccaf1f4df5e80abc8479ba2
Harsh Jain <hershjain@gmail.com>
2024-11-10 20:26:13 -0500

    categories from backend

--- backend/spotify/urls.py
+++ backend/spotify/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .auth_views import get_spotify_auth_url, spotify_callback, get_playlists
+from .auth_views import get_spotify_auth_url, spotify_callback, get_playlists, get_categories
 from .views import user_profile, login_required
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('auth/login', get_spotify_auth_url, name='get_spotify_auth_url'),
     path('auth/callback/', spotify_callback, name='spotify_callback'),
     path('playlists/', get_playlists, name='get_playlists'),
+    path('categories/', get_categories, name='get_categories'),
     path('profile/', user_profile, name='user-profile'),
 ]
     
