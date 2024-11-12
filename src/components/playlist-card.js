9a80b6a18888af7a123db00f2caf0a0b6617b3f2
Shiva Vemireddy <shiva@nirva.ai>
2024-11-11 18:14:57 -0800

    fixed playlist links

--- src/components/playlist-card.js
+++ src/components/playlist-card.js
@@ -3,11 +3,11 @@ import '../styles/playlist-card.css';
 
 const PlaylistCard = ({ title, imageUrl, curator, url }) => {
     return (
-        <div className = "playlist-card" href={url}>
+        <a href={url} target="_blank" rel="noopener noreferrer" className="playlist-card">
             <img src={imageUrl} alt={title} className="playlist-image" />
             <h3 className="playlist-title">{title}</h3>
             <p className="playlist-curator">{curator}</p>
-        </div>
+        </a>
     );
 };
 
