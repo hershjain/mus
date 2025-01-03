a1eec3b293b471053a3673e0a654cfb379ec2568
Shiva Vemireddy <shiva@nirva.ai>
2025-01-02 22:23:02 -0800

    urls.py for check connection

--- backend/spotify/urls.py
+++ backend/spotify/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .auth_views import get_spotify_auth_url, spotify_callback, get_playlists, get_categories, get_catPL, get_spf, get_spuserid, set_imp_playlists, add_playlist_to_lib
+from .auth_views import get_spotify_auth_url, spotify_callback, get_playlists, get_categories, get_catPL, get_spf, get_spuserid, set_imp_playlists, add_playlist_to_lib, get_spotifyconnection
 from .views import user_profile, login_required, set_bio, set_username, pullhh, pub_user_profile, follow_user, pullall, pullpfs, set_toppl, rem_toppl
 
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('set-bio/', set_bio, name='set_bio'),
     path('getspf/', get_spf, name='get_spf'),
     path('getspuserid/', get_spuserid, name='get_spuserid'),
+    path('check-connection/', get_spotifyconnection, name='get_spotifyconnection'),
     path('profile/', user_profile, name='user_profile'),
     path('profile/<str:username>/', pub_user_profile, name='pub_user_profile'),
     path('profile/<str:username>/follow/', follow_user, name='follow_user'),
