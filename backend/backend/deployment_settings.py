42b0664c9f625aa202a1045267c23f25e64cd2fc
Harsh Jain <hershjain@gmail.com>
2025-01-06 00:40:12 -0800

    updates

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -53,9 +53,11 @@ DATABASES = {
     )
 }
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = "/static/"
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+STATIC_ROOT = BASE_DIR / "staticfiles"    
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
