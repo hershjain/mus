25d8da8415229b801eb30b7289c6c2171a8b972a
Harsh Jain <hershjain@gmail.com>
2025-01-06 11:45:48 -0800

    w/agi update

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 # settings_module = 'backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
 
 
-settings_module = 'backend.backend.deployment_settings'
+settings_module = 'backend.deployment_settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_asgi_application()
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # settings_module = 'backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
-settings_module = 'backend.backend.deployment_settings'
+settings_module = 'backend.deployment_settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
